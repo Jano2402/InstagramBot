367407905986ff6a72e91a8f8e01bb7188c2a65c
Alejandro Martínez <martinezalejandrouy7@gmail.com>
2024-04-15 11:34:12 -0300

    first commit

---
+++ gmailTest.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto('https://www.google.com/intl/es/gmail/about/');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
+    await page.setDefaultTimeout(10000);
+    await page.waitForNavigation();
+
+    await page.keyboard.type('Gerardo');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
+    
+    await page.screenshot({ path : 'example.png' });
+    
+    await browser.close();
+  })();
---
+++ igTest.js
@@ -0,0 +1,2 @@
+const puppeteer = require('puppeteer');
+let path = 'https://www.instagram.com/accounts/emailsignup/'
